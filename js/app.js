481949e33c796d06c273e181d23108fd1cc47226
Viraj Chitnis <chitnisviraj@gmail.com>
2014-11-10 14:33:38 -0500

    Using factories for JSON data
    The JSON data is retrieved using Angular services.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,76 @@
 var app = angular.module('app', ['ngRoute', 'mainController']);
 
+app.factory('Products', function(){
+	return [
+	{
+		name: 'Martigues',
+		description: "Description",
+		colors: ["Blanc"],
+		price: 22.00
+	},
+	{
+		name: 'Toscana',
+		description: "Description",
+		colors: ["Vert"],
+		price: 22.00
+	},
+	{
+		name: 'Pergola',
+		description: "Description",
+		colors: ["Anis", "Lavande", "Prune"],
+		price: 22.00
+	},
+	{
+		name: 'Castellane',
+		description: "Description",
+		colors: ["Rouge"],
+		price: 22.00
+	},
+	{
+		name: 'Pavot',
+		description: "Description",
+		colors: [],
+		price: 22.00
+	},
+	{
+		name: 'Cigale',
+		description: "Description",
+		colors: ["Naturel", "Bordeux"],
+		price: 22.00
+	},
+	{
+		name: 'Tournesol',
+		description: "Description",
+		colors: ["Blanc/Vert", "Jaune/Rouge"],
+		price: 22.00
+	},
+	{
+		name: 'Olive',
+		description: "Description",
+		colors: ["Blanc/Vert", "Jaune/Rouge"],
+		price: 22.00
+	},
+	{
+		name: 'Lavande',
+		description: "Description",
+		colors: ["Blanc/Beige", "Jaune/Bleu"],
+		price: 22.00
+	},
+	{
+		name: 'Citron',
+		description: "Description",
+		colors: ["Blanc/Beige", "Jaune/Bleu"],
+		price: 22.00
+	},
+	{
+		name: 'Arbre olivier',
+		description: "Description",
+		colors: ["Jaune vif"],
+		price: 22.00
+	},
+	];
+});
+
 app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 	.when('/home', {
